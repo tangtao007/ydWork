2f79e4c74c58abcdd09f06d68e3305d20f89213e
tangtao <tangtao_mail@sina.com>
2017-11-20 19:13:13 +0800

    express php实战

--- 02/exercise/express02/app.js
+++ 02/exercise/express02/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
 app.use(express.static('public'));
 // 配置swig
@@ -7,9 +8,37 @@ app.set('view engine','html');
 app.engine('html',swig.renderFile);
 // 配置路由
 app.get('/',function(req,res,next){
-	res.render('index');
+	res.render('index',{
+		title:"第一个Express的小程序"
+	});
 });
-
+app.get('/receive',function(req,res,next){
+	var post  = {
+		username: req.query.userName
+	};
+	var query = connection.query('INSERT INTO userinfo SET ?', post, function (error, results, fields) {
+		if (error){
+			console.log(error);
+			res.json({
+				success:"no",
+				msg:"插入失败"
+			});
+		}else {
+			res.json({
+				success:"ok",
+				msg:"插入成功"
+			});
+		};
+		
+	});
+});
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'test'
+	});
+	connection.connect();
 // 容错机制
 app.get("*",function(req,res,next){
 	res.status(404);
--- 02/exercise/express02/public/script/index.js
+++ 02/exercise/express02/public/script/index.js
@@ -1,1 +1,19 @@
-console.log($);
+$(function(argument){
+	$("#btnsubmit").click(function(event){
+		event.preventDefault();
+		$.ajax({
+			url:'/receive',
+			type:'get',
+			dataType:'json',
+			data:{
+				userName:$("#userName").val()
+			},
+			success:function(data){
+				console.log(data);
+			},
+			error:function(){
+				alert('error');
+			}
+		})
+	});
+});
