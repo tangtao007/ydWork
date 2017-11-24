85da9226c5d4bff4fed6fa6c8780911781075d08
tangtao <tangtao@tangtaodeiMac.local>
2017-11-24 20:54:25 +0800

    webpack01

--- 03/exercise/webpack01/src/scripts/app.js
+++ 03/exercise/webpack01/src/scripts/app.js
@@ -1,2 +1,3 @@
 var index = require('./index');
+require('../styles/index');
 index.fn("init init");
--- 03/exercise/webpack01/webpack.config.js
+++ 03/exercise/webpack01/webpack.config.js
@@ -1,8 +1,26 @@
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HTML
 module.exports = {
   entry: __dirname + "/src/scripts/app.js",
   output:{
-    path: __dirname + "/build/scripts",
-    filename:"[name]-[hash].js"
+    path: __dirname + "/build",
+    filename:"./scripts/[name]-[hash].js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: __dirname + '/src/index.htm'
+    })
+  ],
+  module: {
+    //加载器配置
+    loaders: [
+        { test: /\.css$/, loader: 'style-loader!css-loader' }
+    ]
+},
+  //其它解决方案配置
+  resolve: {
+    extensions: ['', '.js', '.css'], 
   }
 }
