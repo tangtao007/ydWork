6d2043b1c53f695346909a8b8af4981240bcfe17
tangtao <tangtao_mail@sina.com>
2018-10-11 23:25:02 +0800

    webpack4基础配置

--- webpack/spa-webpack/src/components/sync/index.js
+++ webpack/spa-webpack/src/components/sync/index.js
@@ -3,7 +3,7 @@ import {isArray} from "lodash-es"
 import item from './sync.css';
 const sync = function(){
   console.log("sync");
-  document.getElementById("app").innerHTML = `<ch1 class="${item.test}">我是样式</ch1>`;
+  //document.getElementById("app").innerHTML = `<ch1 class="${item.test}">Hello,我是样式!</ch1>`;
 }
 
 const isCheckArray = function(args){
--- webpack/spa-webpack/webpack.config.js
+++ webpack/spa-webpack/webpack.config.js
@@ -1,14 +1,51 @@
 const WebpackDeepScopeAnalysisPlugin = require('webpack-deep-scope-plugin').default;
-module.exports = {
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const argv = require('yargs-parser')(process.argv.slice(2));
+const merge = require('webpack-merge');
+const _mode = argv.mode || "development";
+const _modelflag = (_mode == "production" ? true : false);
+const _mergeConfig = require(`./config/webpack.${_mode}.js`);
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PurifyCSSPlugin = require('purifycss-webpack');
+const glob = require('glob');
+const {
+  join
+} = require('path');
+const setIterm2Badge = require('set-iterm2-badge');
+setIterm2Badge("开发环境");
+webpackConfig = {
   module: {
     rules: [{
       test: /\.css$/,
-      use: ['style-loader', {
-        loader: 'css-loader?modules'
-      }]
+      use: [{
+          loader: MiniCssExtractPlugin.loader
+        },
+        //'style-loader', 
+        {
+          //loader: 'css-loader?modules&localIdentName=[name]_[local]-[hash:base64:5]'
+          loader: 'css-loader'
+        }
+      ]
     }]
   },
   plugins: [
     new WebpackDeepScopeAnalysisPlugin(),
+    new MiniCssExtractPlugin({
+      filename: _modelflag?'styles/[name].[hash:5].css':"styles/[name].css",
+      chunkFilename: _modelflag?'style/[id].[hash:5].css':"style/[id].css",
+    }),
+    new HtmlWebpackPlugin({
+      filename:"index.html",
+      template:"src/index.html"
+    }),
+    new CleanWebpackPlugin(["dist"]),
+    // new PurifyCSSPlugin({
+    //   // Give paths to parse for rules. These should be absolute!
+    //   paths: glob.sync(join(__dirname, './dist/*.html')),
+    // })
   ],
 }
+
+module.exports = merge(_mergeConfig, webpackConfig);
